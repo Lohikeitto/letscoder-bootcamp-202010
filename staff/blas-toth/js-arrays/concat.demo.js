e8678e29d86b4cfd967d34b7646b874f3dad45c6
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-17 11:05:53 +0200

    add concat.demo

---
+++ staff/blas-toth/js-arrays/concat.demo.js
@@ -0,0 +1,8 @@
+console.log('DEMO concat');
+
+console.log("Merge array1 and array2 into a third array")
+
+const array1 = ['a', 'b', 'c', '5555'];
+const array2 = ['d', 'e', 'f', 'g', 'h', 'i'];
+
+console.log(concat(array1, array2));
