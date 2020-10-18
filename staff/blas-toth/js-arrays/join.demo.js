5ec80ea2fa4d1b4904f074c7e78718a31715b030
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-18 12:39:47 +0200

    add join.demo.js

---
+++ staff/blas-toth/js-arrays/join.demo.js
@@ -0,0 +1,5 @@
+console.log('DEMO join');
+var months = ['January', 'February', 'March', 'April', 'May', 'June'];
+console.log("Write the elements of the months array to the log separated by two hyphens (--).");
+
+console.log(join(months, "--"));
