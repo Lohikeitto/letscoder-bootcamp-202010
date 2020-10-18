6c1e700fcdec303b92f9a86ec93722a15b989283
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-18 18:55:28 +0200

    add slice.demo.js

---
+++ staff/blas-toth/js-arrays/slice.demo.js
@@ -0,0 +1,10 @@
+console.log("DEMO slice");
+
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+console.log(fruits);
+
+console.log("Slice the array from the 0th element to the 3rd (excluded) and save it to a new array!")
+
+result = slice(fruits, 0, 3);
+
+console.log(result);
