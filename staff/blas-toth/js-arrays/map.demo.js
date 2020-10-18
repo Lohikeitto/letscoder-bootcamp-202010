c11bd8ab35e7b7234cf035fda977ca6598e1ab64
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-18 14:17:15 +0200

    add map.demo.js

---
+++ staff/blas-toth/js-arrays/map.demo.js
@@ -0,0 +1,12 @@
+console.log('DEMO map');
+
+console.log('return a new array with the elements that function returns');
+
+var arr = [1,2,3,4];
+
+var result = map(arr, function(value) {
+    return value *2;
+})
+
+console.log(result);
+
