7d096ed43ac527184d92900de36c20603df076a7
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-18 15:23:43 +0200

    add pop + pop.demo.js

---
+++ staff/blas-toth/js-arrays/pop.demo.js
@@ -0,0 +1,11 @@
+console.log('DEMO pop')
+
+var cities = ['Barcelona', 'Madrid', 'Valencia', 'Zaragoza'];
+
+console.log("Remove the last element of the array cities");
+
+console.log(cities);
+pop(cities);
+console.log(cities);
+
+
---
+++ staff/blas-toth/js-arrays/pop.js
@@ -0,0 +1,6 @@
+function pop(array) {
+        console.log(array[array.length-1]);
+        array.length = array.length - 1;
+    }
+
+
