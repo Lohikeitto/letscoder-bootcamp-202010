2e7704c38b0bed0055cb85f14743736407a1f6f2
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-18 16:50:39 +0200

    add push.demo.js

---
+++ staff/blas-toth/js-arrays/push.demo.js
@@ -0,0 +1,9 @@
+console.log("DEMO push");
+
+console.log("Add the element: 'Miami' to the array called cities");
+
+var cities = ["Madrid", "Barcelona", "Sevilla", "Valencia", "Zaragoza"];
+
+push(cities, "Miami");
+
+console.log(cities);
