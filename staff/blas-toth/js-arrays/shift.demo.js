afd728b71899590a3f7be13cfc89e45cbf628204
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-18 16:38:17 +0200

    add shift.demo.js

---
+++ staff/blas-toth/js-arrays/shift.demo.js
@@ -0,0 +1,9 @@
+console.log('DEMO shift')
+
+var films = ['Pulp Fiction', 'Lord of the Rings', 'The Number 23', 'Taxi'];
+
+console.log("Remove the first element of the array films");
+
+console.log(films);
+shift(films);
+console.log(films);
