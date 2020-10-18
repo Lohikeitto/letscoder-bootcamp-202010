0b7927cf76b6980918b48276ef96d7466e72f9af
Lohikeitto <balazstothlaszlo@gmail.com>
2020-10-18 19:23:42 +0200

    add tostring.demo.js

---
+++ staff/blas-toth/js-arrays/tostring.demo.js
@@ -0,0 +1,9 @@
+console.log('DEMO tostring');
+
+console.log("Return the elements of an array as a string");
+
+var arrayz = ["Bill", "Charlie", true, 1, 2, "a", "1a"];
+
+console.log(arrayz);
+
+toString(arrayz);
